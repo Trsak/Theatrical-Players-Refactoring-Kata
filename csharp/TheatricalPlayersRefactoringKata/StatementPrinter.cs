9edd65a9fdf936bf1741c21560a5b4c1b315e550
tompsota <to.psota@gmail.com>
2022-04-06 16:48:22 +0200

    Added dictionary

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -12,6 +12,7 @@ namespace TheatricalPlayersRefactoringKata
             var volumeCredits = 0;
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             var cultureInfo = new CultureInfo("en-US");
+            var performanceResults = new Dictionary<string, decimal>();
 
             foreach(var perf in invoice.Performances) 
             {
@@ -34,10 +35,12 @@ namespace TheatricalPlayersRefactoringKata
                 // add extra credit for every ten comedy attendees
                 if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
+                performanceResults.Add(perf.PlayID, Convert.ToDecimal(thisAmount / 100));
                 // print line for this order
                 result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
             }
+            
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += string.Format("You earned {0} credits\n", volumeCredits);
             return result;
